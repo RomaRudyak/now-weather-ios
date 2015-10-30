7276e299b8a3736bfdf318baa419212af3a38997
Roman.Rudyak <roma.rudyak@gmail.com>
2015-10-30 15:21:45 +0300

    Implement basic UI for iPhones landscape

---
+++ Now-weather/RoundUIView.swift
@@ -0,0 +1,33 @@
+//
+//  RoundUIView.swift
+//  Now-weather
+//
+//  Created by Admin on 10/28/15.
+//  Copyright © 2015 Roman Rudyak. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class RoundUIView: UIView {
+
+	override func awakeFromNib() {
+		clipsToBounds = true
+		applayRadius()
+	}
+	
+	override func layoutSubviews() {
+		applayRadius()
+		super.layoutSubviews()
+	}
+	
+	private func applayRadius(){
+		let w = frame.size.width
+		let h = frame.size.height
+		
+		let radius = max(w / 2, h / 2)
+		
+		layer.cornerRadius = radius
+	}
+	
+}
